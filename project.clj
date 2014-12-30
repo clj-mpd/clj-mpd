38cf3f0ac5e3b5bb28f515a5827d08fd407e6f1f
Dave Yarwood <dave.yarwood@gmail.com>
2014-12-30 17:53:41 -0500

    update clojure version and .gitignore

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-mpd "0.0.1-SNAPSHOT"
   :description "Clojure library to interface with MPD (wrapping javampd)"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.bff.javampd/javampd "4.0"]
                  [clj-wallhack "1.0"]]
-  :main "clj-mpd.test_app/-main")
+  :main clj-mpd.test_app)
