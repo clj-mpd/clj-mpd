e3ee2834a137609169de0c77ef013f2727278be5
Dave Yarwood <dave.yarwood@gmail.com>
2015-01-05 00:13:36 -0500

    add MPDAlbum wrapper

---
+++ src/clj_mpd/item/album.clj
@@ -0,0 +1,15 @@
+(ns ^{:author "Dave Yarwood"
+      :doc "MPDAlbum related functions for clj-mpd"}
+  clj-mpd.item.album)
+
+(comment
+  "MPDAlbum has two attributes: the (album) name and the artistName. Whereas
+   artistName has explicit getter and setter methods, the album name is an
+   implicit part of the MPDAlbum object -- the getter is just calling
+   .toString(), and the setter is supplying the album name as the single
+   argument when instantiating an MPDAlbum.")
+
+(defn get-artist
+  "Get the artist name associated with the album object."
+  [album]
+  (.getArtistName album))
