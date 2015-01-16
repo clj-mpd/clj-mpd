b8c3f6ef75b549a0ba2b0daa57dde3d89f207dc8
Dave Yarwood <dave.yarwood@gmail.com>
2015-01-16 00:25:22 -0500

    MPD instance tests

--- test/clj_mpd/test/core.clj
+++ test/clj_mpd/test/core.clj
@@ -2,5 +2,12 @@
   (:use [clj-mpd.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+; these tests rely on there being an MPD instance running on localhost:6600
+
+(deftest mpd-instance-tests
+  (with-mpd-connection :default
+    (testing "attributes"
+      (is (re-matches #"MPD [\d\w\.\-]+" (get-version)))
+      (is (= "localhost" (.getHostName (get-address))))
+      (is (= 6600 (get-port)))
+      (is (= 0 (get-timeout))))))
